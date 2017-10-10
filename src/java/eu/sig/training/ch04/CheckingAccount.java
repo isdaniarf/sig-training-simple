8deb040a9f09b5cae1118a43f87aec2e41553711
slim_shady <musaffa.affa@gmail.com>
2017-10-10 13:27:13 +0700

    commit 3

--- src/java/eu/sig/training/ch04/CheckingAccount.java
+++ src/java/eu/sig/training/ch04/CheckingAccount.java
@@ -12,6 +12,7 @@ public class CheckingAccount {
             throw new BusinessException("Limit exceeded!");
         }
         // 2. Assuming result is 9-digit bank account number, validate 11-test:
+        /*
         int sum = 0;
         for (int i = 0; i < counterAccount.length(); i++) {
             char character = counterAccount.charAt(i);
@@ -26,6 +27,17 @@ public class CheckingAccount {
         } else {
             throw new BusinessException("Invalid account number!");
         }
+        */
+
+        if (Accounts.isValid(counterAccount)) {
+        // 2. Look up counter account and make transfer object:
+        CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
+        Transfer result = new Transfer(this, acct, amount);
+        return result;
+        } else {
+        throw new BusinessException("Invalid account number!");
+        }
+
     }
 
     public void addInterest() {
