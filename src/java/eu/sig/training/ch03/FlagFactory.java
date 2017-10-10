78467ce076aa5198044cdfb5c09b14722eb87769
slim_shady <musaffa.affa@gmail.com>
2017-10-10 13:36:38 +0700

    commit 4

--- src/java/eu/sig/training/ch03/FlagFactory.java
+++ src/java/eu/sig/training/ch03/FlagFactory.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FlagFactory {
-
+/*
     public List<Color> getFlagColors(Nationality nationality) {
         List<Color> result;
         switch (nationality) {
@@ -45,6 +45,23 @@ public class FlagFactory {
             break;
         }
         return result;
+    }*/
+
+    private static Map<Nationality, List<Color>> FLAGS =
+    new HashMap<Nationality, List<Color>>();
+
+    static {
+        FLAGS.put(DUTCH, Arrays.asList(Color.RED, Color.WHITE, Color.BLUE));
+        FLAGS.put(GERMAN, Arrays.asList(Color.BLACK, Color.RED, Color.YELLOW));
+        FLAGS.put(BELGIAN, Arrays.asList(Color.BLACK, Color.YELLOW, Color.RED));
+        FLAGS.put(FRENCH, Arrays.asList(Color.BLUE, Color.WHITE, Color.RED));
+        FLAGS.put(ITALIAN, Arrays.asList(Color.GREEN, Color.WHITE, Color.RED));
     }
+
+    public List<Color> getFlagColors(Nationality nationality) {
+    List<Color> colors = FLAGS.get(nationality);
+    return colors != null ? colors : Arrays.asList(Color.GRAY);
+    }
+
 
 }
